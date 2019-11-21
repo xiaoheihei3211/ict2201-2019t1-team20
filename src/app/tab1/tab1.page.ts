267204d67bad2397866cf4feb65e51b49e0aa40a
matt4realz <43530617+matt4realz@users.noreply.github.com>
2019-11-22 01:21:14 +0800

    Add files via upload

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
 import { CrudService } from './../services/crud.service';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -12,9 +13,13 @@ export class Tab1Page implements OnInit {
 
     reward : any
     rewardList : any
+    rewardSub : any
     steps : number;
     userID : string = "";
-    abcd: number;
+    confirmation : string = "";
+    selectedReward : string = "";
+    i : number;
+
 
     userTier : string = "";
     userTierNo : number;
@@ -27,11 +32,51 @@ export class Tab1Page implements OnInit {
 
     displayArray = []
 
-    collectedReward : string = "";
-
+    collectedReward = "";
     checkReward : boolean = false;
 
-    constructor(private changeRef: ChangeDetectorRef, private crudService: CrudService) {}
+    constructor(private changeRef: ChangeDetectorRef, private crudService: CrudService,
+    public alertController: AlertController) {}
+
+    async successAlert() {
+      const alert = await this.alertController.create({
+        header : "Success!",
+        message : "Reward Successfully Added",
+        buttons : ['OK']
+      });
+      await alert.present();
+    }
+
+    async displayRewardAlert(){
+      const alert = await this.alertController.create({
+        header: "Your Claimed Rewards",
+        message : this.displayArray + '\n',
+        buttons : ['OK']
+      });
+      await alert.present()
+    }
+
+    async wrongTierAlert() {
+      const alert = await this.alertController.create({
+        header : "OH NO!",
+        message : "This Reward Is Above Your Tier! Please Walk More!",
+        buttons : ['OK']
+      });
+
+      await alert.present();
+    }
+
+    async pointsAlert() {
+    const alert = await this.alertController.create({
+      header : "OH NO!",
+      message : "You Do Not Have Enough Points! Please Walk More!",
+      buttons : ['OK']
+    });
+
+    await alert.present();
+    }
+
+
 
     ngOnInit() {
      this.crudService.read_Reward().subscribe(data => {
@@ -53,7 +98,7 @@ export class Tab1Page implements OnInit {
 
      });
 
-       this.crudService.read_Bronze().subscribe(data => {
+      this.crudService.read_Bronze().subscribe(data => {
          this.rewardList = data.map(f => {
            return {
              id: f.payload.doc.id,
@@ -65,13 +110,11 @@ export class Tab1Page implements OnInit {
          })
          console.log(this.rewardList);
 
-
           this.bronzeArray[0] = this.rewardList[0].id
           this.bronzeArray[1] = this.rewardList[0].grab
           this.bronzeArray[2] = this.rewardList[0].spca
           this.bronzeArray[3] = this.rewardList[0].watsons
           this.bronzeArray[4] = this.rewardList[0].points
-
 
           this.silverArray[0] = this.rewardList[1].id
           this.silverArray[1] = this.rewardList[1].grab
@@ -84,26 +127,44 @@ export class Tab1Page implements OnInit {
           this.goldArray[2] = this.rewardList[2].spca
           this.goldArray[3] = this.rewardList[2].watsons
           this.goldArray[4] = this.rewardList[2].points
-
        });
-
-
 
    }
 
-   displayRedeemedReward(){
-      do{
-        if(this.reward == ""){
-          alert("You currently have no rewards redeemed")
-          continue;
-        }
-        alert (this.userTier)
-        break;
-      }while(this.checkReward == true)
-
+    loadRedeemedReward(){
+        this.crudService.read_subcollection(this.userID).subscribe(data=>{
+          this.rewardSub = data.map(g=>{
+            return{
+              id: g.payload.doc.id,
+              claimed: g.payload.doc.data()['reward'],
+            };
+          });
+          console.log(this.rewardSub);
+        })
     }
 
-    validateUserReward(rewardTierNo:number){
+
+
+    displayRedeemedReward(){
+      this.i = 0;
+      if(this.i < 1){
+        this.loadRedeemedReward();
+        this.i = 1;
+      }
+
+      for(var i=0; i < Object.keys(this.rewardSub).length; i++){
+        this.displayArray[i] = this.rewardSub[i].claimed;
+      }
+
+      this.displayRewardAlert()
+    }
+
+
+    validateUserReward(rewardTierNo:number, rewardNo:number){
+
+
+
+      //Check User Tier
       if(this.userTier == "Bronze"){
         this.userTierNo = 1;
       }else if (this.userTier == "Silver"){
@@ -112,6 +173,29 @@ export class Tab1Page implements OnInit {
         this.userTierNo = 3;
       }
 
+      //Check Selected Reward
+      if(rewardNo == 1){
+        this.selectedReward = this.bronzeArray[1];
+      }else if(rewardNo == 2){
+        this.selectedReward = this.silverArray[1];
+      }else if(rewardNo == 3){
+        this.selectedReward = this.goldArray[1];
+      }else if(rewardNo == 4){
+        this.selectedReward = this.bronzeArray[3];
+      }else if(rewardNo == 5){
+        this.selectedReward = this.silverArray[3];
+      }else if(rewardNo == 6){
+        this.selectedReward = this.goldArray[3];
+      }if(rewardNo == 7){
+        this.selectedReward = this.bronzeArray[2];
+      }else if(rewardNo == 8){
+        this.selectedReward = this.silverArray[2];
+      }else if(rewardNo == 9){
+        this.selectedReward = this.goldArray[2];
+      }
+
+      alert(this.selectedReward)
+      //check selected reward tier & set points
       if(rewardTierNo == 1){
         this.ptsToDeduct = this.bronzeArray[4];
       }
@@ -121,16 +205,19 @@ export class Tab1Page implements OnInit {
         this.ptsToDeduct = this.goldArray [4];
       }
 
+      //check for sufficient steps, if sufficient, pass
       if(this.steps >= this.ptsToDeduct){
         if(this.userTierNo < rewardTierNo){
-          alert("This Reward Is Above Your Tier! Please Walk More!")
+          this.wrongTierAlert();
         }else if(this.userTierNo > rewardTierNo){
           this.redeemReward(this.ptsToDeduct)
+          this.CreateRecord(this.userID);
         }else{
           this.redeemReward(this.ptsToDeduct)
+          this.CreateRecord(this.userID);
         }
       }else{
-        alert("Not Enough Points! Please Walk More!")
+        this.pointsAlert();
       }
 
     }
@@ -140,7 +227,22 @@ export class Tab1Page implements OnInit {
       this.steps = this.steps - deductPoint
       reward['steps'] = this.steps;
       this.crudService.update_Reward(this.userID, reward)
+      this.confirmation = "yes"
+      if (this.confirmation == "yes"){
+        this.successAlert();
+      }
     }
+
+    CreateRecord(uid) {
+    let record = {};
+    record['reward'] = this.selectedReward
+    this.crudService.add_Reward(record, uid).then(resp => {
+      console.log(resp);
+    })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
 
 
